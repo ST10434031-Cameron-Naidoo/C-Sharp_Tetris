c2fdb9af5a9eb38d75ddedd2eeae1620386ae137
ST10434031-Cameron-Naidoo <st10434031@myemeris.edu.za>
2026-07-14 14:56:31 +0200

    added position class to set and get columns, and set position

---
+++ 011DB_Tetris/Position.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _011DB_Tetris
+{
+    class Position
+    {
+        public int row;
+        public int column;
+
+
+        //traditional getters and setters
+        public int getRow()
+        {
+            return row;
+        }
+
+        public int getColumns()
+        {
+            return getColumns();
+        }
+
+        public void setRow(int r)
+        {
+            row = r;
+        }
+
+        public void setColumns(int c)
+        {
+            column = c;
+        }
+
+        public Position(int r, int c)
+        {
+            row = r;
+            column = c;
+        }
+
+    }
+}
